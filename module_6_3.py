b68157d21339bd8ff0bd309c9a7b487eccca55b0
Roman-Machtakov <79520337274@mail.ru>
2024-08-27 14:59:51 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,44 +1,33 @@
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
         self.x_distance += dx
-        return self.x_distance
-
-
-class Horse:
-    x_distance = 0
-    sound = 'Frrr'
-
-    def run(self, dx):
-        self.x_distance += dx
-        return self.x_distance
-
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
-        return self.y_distance
-
 
 class Pegasus(Horse, Eagle):
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
-        tpl = (self.x_distance, self.y_distance)
-        return tpl
+        return (self.x_distance, self.y_distance)
 
     def voice(self):
-        print(f'{self.sound}')
-
+        print(self.sound)
 
 p1 = Pegasus()
 
@@ -47,4 +36,3 @@ p1.move(10, 15)
 print(p1.get_pos())
 p1.move(-5, 20)
 print(p1.get_pos())
-p1.voice()
